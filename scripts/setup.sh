811ad3ec8c3d920470b76b37d0be323310b79bda
Andrew Olney <aolney@memphis.edu>
2016-01-03 19:50:33 -0600

    working frontpage, need to finish timeline, add agency sections and work blog into publications, etc

---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+#for personal theme
+#ubuntu/debian specific
+#sudo apt-get install ruby-full
+gem install jekyll jekyll-paginate jemoji html-proofer
+#now run ./scripts/serve-production
+
+#from some random guide
+#sudo apt-get install ruby-full
+#gem install jekyll
+#gem install github-pages
+
+#from http://jekyllrb.com/
+#gem install jekyll
+#jekyll new my-awesome-site
+#cd my-awesome-site
+#jekyll serve
+# => Now browse to http://localhost:4000 
